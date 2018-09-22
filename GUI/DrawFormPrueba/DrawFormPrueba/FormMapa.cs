b270f6a29c3236268c95924c7707b9e3fd076dac
u201314147 <u201314147@upc.edu.pe>
2018-09-21 19:23:02 -0500

    Agregado lineas de viaje en mapa

--- GUI/DrawFormPrueba/DrawFormPrueba/FormMapa.cs
+++ GUI/DrawFormPrueba/DrawFormPrueba/FormMapa.cs
@@ -16,19 +16,32 @@ namespace DrawFormPrueba
         int aux2 = 0; int aux3 = 0;
         int A, S, D, W, ADD, SUB = 0;
         int LE = 1;
+
         float XW = 15.0f;
         float YW = 15.0f;
         float scale = 1.0f;
-        int lenght = 72000;
-        int lenghtlineas = 72000;
+        int lenght = 145225;
+        int lenghtlineas = 145225;
+        int auxLineas = 0;
+
         Random r = new Random();
         Rectangle mapa = new Rectangle();
         List<Puntos> apuntadores = new List<Puntos>();
         List<Puntos> puntos = new List<Puntos>();
         List<Lineas> lineas = new List<Lineas>();
         List<String> nombres = new List<String>();
+
+        //punto de inicio para el algoritmo de union aleatoria
+        Puntos randomprev = new Puntos(0,0,0,0,0);
+        int auxLineas2 = 0;
         public FormMapa()
         {
+
+             XW = 7667.456f;
+             YW = -802.6277f;
+
+            randomprev = new Puntos(0, 0, r.Next(0, 255), r.Next(0, 255), r.Next(0, 255));
+
             InitializeComponent();
             mapa.X = 0;
             mapa.Y = 0;
@@ -51,63 +64,72 @@ namespace DrawFormPrueba
             for (int i = 0; i < lenghtlineas; i++)
             {
                
-                Lineas linea = new Lineas(0, 4, 0, 4);
+                Lineas linea = new Lineas();
                 lineas.Add(linea);
 
             }
             leerArchivoAlMap();
-           // leerLineasAlMap();
+           leerLineasAlMap();
         }
         void leerLineasAlMap()
         {
 
-            string line = "";
-            Char delimiter = ';';
-          
-            int maximo = 0;
-            int aum = 0;
-            string[] datos;
-            // textBox1.Text = "";
-            using (StreamReader sr = new StreamReader(@"matrix.al"))
+           
+                string line = "";
+                Char delimiter = ';';
 
-                while (maximo < lenghtlineas - 1)
-                {
-
-                    line = sr.ReadLine();
-                    datos = line.Split(delimiter);
-
-                    maximo++;
+                int maximo = 0;
+                int aum = 0;
+                string[] datos;
+              
 
 
 
-                    lineas.ElementAt(aum).setX1(puntos.FirstOrDefault(x => x.getId() == Convert.ToInt32(datos[0])).getX());
-                         lineas.ElementAt(aum).setY1(puntos.FirstOrDefault(x => x.getId() == Convert.ToInt32(datos[0])).getY());
-                          lineas.ElementAt(aum).setX2(puntos.FirstOrDefault(x => x.getId() == Convert.ToInt32(datos[2])).getX());
-                          lineas.ElementAt(aum).setY2(puntos.FirstOrDefault(x => x.getId() == Convert.ToInt32(datos[2])).getY());
-                   
 
+
+                using (StreamReader sr = new StreamReader(@"matrix.al"))
+
+                    while (maximo < lenghtlineas - 1)
+                    {
+
+                        line = sr.ReadLine();
+                        datos = line.Split(delimiter);
+
+                        maximo++;
+
+
+
+
+                        lineas.ElementAt(aum).setId(Convert.ToInt32(datos[0]));
+                        lineas.ElementAt(aum).setId1(Convert.ToInt32(datos[2]));
+                        lineas.ElementAt(aum).setId2(Convert.ToInt32(datos[4]));
+                        lineas.ElementAt(aum).setId3(Convert.ToInt32(datos[6]));
+                        lineas.ElementAt(aum).setId4(Convert.ToInt32(datos[8]));
                     aum++;
-                }
+                    }
+        
 
 
         }
 
         void leerArchivoAlMap()
         {
+            
+                string line = "";
+                Char delimiter = ';';
+                int maximo = 0;
+                int aum = 0;
+                string[] datos;
+                string[] datos2;
 
-            string line = "";
-            Char delimiter = ',';
-            int maximo = 0;
-            int aum = 0;
-            string[] datos;
-            // textBox1.Text = "";
-            using (StreamReader sr = new StreamReader(@"data.csv"))
+            using (StreamReader sr = new StreamReader(@"metadata.md"))
 
-                while (maximo < lenght - 1)
-                {
-                    line = sr.ReadLine();
+                    while (maximo < lenght - 1)
+                    {
                     
-                    line = sr.ReadLine();
+
+                    if(lenght< 72613)
+                    {   line = sr.ReadLine();
                         datos = line.Split(delimiter);
                         maximo++;
 
@@ -116,13 +138,81 @@ namespace DrawFormPrueba
                         puntos.ElementAt(aum).setX(float.Parse(datos[2]) * 100);
                         puntos.ElementAt(aum).setY(float.Parse(datos[3]) * -100);
 
-                     
-                    aum++;
-                }
-            
-       
+                        aum++;}
+
+                    if (lenght > 72613)
+                    {
+                        line = sr.ReadLine();
+                        datos2 = line.Split(delimiter);
+                        maximo++;
+
+                        puntos.ElementAt(aum).setId(Convert.ToInt32(datos2[0]));
+                        puntos.ElementAt(aum).setNombre(datos2[1]);
+                        puntos.ElementAt(aum).setX(float.Parse(datos2[2]) * 100);
+                        puntos.ElementAt(aum).setY(float.Parse(datos2[3]) * -100);
+
+                        aum++;
+                    }
+
+
+                    }
+
         }
 
+        void randomDrawLineas()
+        {
+            Puntos puntorandom1 = puntos.ElementAt(r.Next(0, lenght));
+            Puntos puntorandom2 = puntos.ElementAt(r.Next(0, lenght));
+
+
+            int linerandom = r.Next(0, lenghtlineas);
+
+
+            lineas.ElementAt(linerandom).setX(puntorandom1.getX());
+            lineas.ElementAt(linerandom).setY(puntorandom1.getY());
+            lineas.ElementAt(linerandom).setX1(puntorandom2.getX());
+            lineas.ElementAt(linerandom).setY1(puntorandom2.getY());
+
+         
+        }
+        void randomDrawLineasContinuo()
+        {
+            Puntos randomnext = puntos.ElementAt(r.Next(0, lenght));
+          
+            lineas.ElementAt(auxLineas2).setX(randomprev.getX());
+            lineas.ElementAt(auxLineas2).setY(randomprev.getY());
+            lineas.ElementAt(auxLineas2).setX1(randomnext.getX());
+            lineas.ElementAt(auxLineas2).setY1(randomnext.getY());
+
+            randomprev = randomnext;
+
+            if (auxLineas2 < lenghtlineas)
+            {
+                auxLineas2++;
+            }
+        }
+        void unirpuntosCercanos()
+        {
+            try
+            {
+                label2.Text = lineas.ElementAt(auxLineas).getId().ToString();
+
+                lineas.ElementAt(auxLineas).setX(puntos.Find(x => x.getId() == lineas.ElementAt(auxLineas).getId()).getX());
+                lineas.ElementAt(auxLineas).setX1(puntos.Find(x => x.getId() == lineas.ElementAt(auxLineas).getId1()).getX());
+                // lineas.ElementAt(auxLineas).setX2(puntos.Find(x => x.getId() == lineas.ElementAt(auxLineas).getId2()).getX());
+                //  lineas.ElementAt(auxLineas).setX3(puntos.Find(x => x.getId() == lineas.ElementAt(auxLineas).getId3()).getX());
+                //   lineas.ElementAt(auxLineas).setX4(puntos.Find(x => x.getId() == lineas.ElementAt(auxLineas).getId4()).getX());
+
+                lineas.ElementAt(auxLineas).setY(puntos.Find(x => x.getId() == lineas.ElementAt(auxLineas).getId()).getY());
+                lineas.ElementAt(auxLineas).setY1(puntos.Find(x => x.getId() == lineas.ElementAt(auxLineas).getId1()).getY());
+                //   lineas.ElementAt(auxLineas).setY2(puntos.Find(x => x.getId() == lineas.ElementAt(auxLineas).getId2()).getY());
+                //   lineas.ElementAt(auxLineas).setY3(puntos.Find(x => x.getId() == lineas.ElementAt(auxLineas).getId3()).getY());
+                //   lineas.ElementAt(auxLineas).setY4(puntos.Find(x => x.getId() == lineas.ElementAt(auxLineas).getId4()).getY());
+            }
+            catch (Exception e)
+            { }
+            auxLineas++;
+        }
         private void FormMapa_KeyPress(object sender, KeyPressEventArgs e)
         {
         
@@ -130,19 +220,13 @@ namespace DrawFormPrueba
 
         private void timer1_Tick_1(object sender, EventArgs e)
         {
-           
+            //unirpuntosCercanos();
+            
             try
             {
-
-                Puntos punto = puntos.ElementAt(r.Next(0, lenght));
-                Puntos punto2 = puntos.ElementAt(r.Next(0, lenght));
+             
 
 
-                int linerandom = r.Next(0, lenghtlineas);
-                lineas.ElementAt(linerandom).setX1(punto.getX());
-                lineas.ElementAt(linerandom).setY1(punto.getY());
-                lineas.ElementAt(linerandom).setX2(punto2.getX());
-                lineas.ElementAt(linerandom).setY2(punto2.getY());
 
                 if (A == 1)            //if (pX + XW < 400)
                 {
@@ -194,21 +278,43 @@ namespace DrawFormPrueba
                 {
                     l.dibujar(buffer.Graphics, XW * -1, YW * -1, scale, ClientSize.Width, ClientSize.Height);
                 }
-                
+
+
+
+              
+                  
 
 
                 buffer.Render();
+
+
 
 
             } catch (Exception g)
             {
 
             }
+
+           
         }
 
         private void FormMapa_KeyDown(object sender, KeyEventArgs e)
         {
-            
+            if (e.KeyCode == Keys.U)
+            {
+                unirpuntosCercanos();
+
+            }
+            if (e.KeyCode == Keys.R)
+            {
+                randomDrawLineas();
+
+            }
+            if (e.KeyCode == Keys.O)
+            {
+                randomDrawLineasContinuo();
+
+            }
             if (e.KeyCode == Keys.A)
             {
                 A = 1;
@@ -259,6 +365,7 @@ namespace DrawFormPrueba
             }
 
             label1.Text = "XW: " + ((XW * -1)/100).ToString() + "  YW:" + ((YW) / 100).ToString() + "  Escala: " + scale;
+            label2.Text = "XWR:" + XW + " YW: " + YW;
 
 
 
@@ -266,18 +373,7 @@ namespace DrawFormPrueba
 
         private void FormMapa_KeyUp(object sender, KeyEventArgs e)
         {
-            if (e.KeyCode == Keys.L)
-            {
-                if (LE == 1)
-                {
-                    LE = 0;
-                }
-                else
-                {
-                    LE = 1;
-                }
-            }
-
+            
             if (e.KeyCode == Keys.A)
             {
                 A = 0;
--- GUI/DrawFormPrueba/DrawFormPrueba/Lineas.cs
+++ GUI/DrawFormPrueba/DrawFormPrueba/Lineas.cs
@@ -10,68 +10,88 @@ namespace DrawFormPrueba
 {
     class Lineas
     {
-        float x1 = 0;
-        float y1 = 0;
-        float x2 = 5;
-        float y2 = 5;
-        public Lineas(float px1, float px2 , float py1, float py2)
+        int id, id1, id2, id3, id4 = 0;
+
+        float x, y, x1, x2, x3, x4, y1, y2, y3, y4 = 0;
+        public Lineas()
         {
-             x1 = px1;
-             y1 = py1;
-             x2 = px2;
-             y2 = py2;
+            id = 0;
+            id1 = 0;
+            id2 = 0;
+            id3 = 0;
+            id4 = 0;
+            x = 0;
+            y = 0;
+            x1 = 0;
+            x2 = 0;
+            x3 = 0;
+            x4 = 0;
+            y1 = 0;
+            y2 = 0;
+            y3 = 0;
+            y4 = 0;
         }
 
        public void dibujar(Graphics g, float XW, float YW, float scale, int width, int height)
         {
-            if ((this.getX1() > XW && this.getX1() < XW + width / scale && this.getY1() > YW && this.getY1() < YW + height / scale) ||
-               (this.getX2() > XW && this.getX2() < XW + width / scale && this.getY2() > YW && this.getY2() < YW + height / scale))
+            if ((this.getX() > XW && this.getX() < XW + width / scale && this.getY() > YW && this.getY() < YW + height / scale) ||
+               (this.getX1() > XW && this.getX1() < XW + width / scale && this.getY1() > YW && this.getY1() < YW + height / scale))
             {
               //  if (scale > 5)
                // { 
                     Pen pen = new Pen(Color.Red, 0.1f);
-                
-                    g.DrawLine(pen, x1, y1, x2, y2);
-              //   }
+
+                if (x1 != 0 && y1 != 0)
+                    g.DrawLine(pen, x, y, x1, y1);
+
+                if(x2 !=0 && y2!=0)
+                g.DrawLine(pen, x, y, x2, y2);
+
+                if (x3 != 0 && y3 != 0)
+                    g.DrawLine(pen, x, y, x3, y3);
+
+                if (x4 != 0 && y4 != 0)
+                    g.DrawLine(pen, x, y, x4, y4);
+
+                //   }
             }
         }
-        
 
 
-        public void setX1(float pX)
-        {
-           x1 = pX;
-        }
-        public void setY1(float pY)
-        {
-            y1 = pY;
-        }
-        public float getX1()
-        {
-            return x1;
-        }
-        public float getY1()
-        {
-            return y1;
-        }
+        public void setX(float pX) { x = pX; }
+        public void setX1(float pX) { x1 = pX;}
+        public void setX2(float pX) { x2 = pX; }
+        public void setX3(float pX) { x3 = pX; }
+        public void setX4(float pX) { x4 = pX; }
+      
+        public void setY(float pY){ y = pY;}
+        public void setY1(float pY) { y1 = pY; }
+        public void setY2(float pY) { y2 = pY; }
+        public void setY3(float pY) { y3 = pY; }
+        public void setY4(float pY) { y4 = pY; }
 
-        public void setX2(float pX)
-        {
-            x2 = pX;
-        }
-        public void setY2(float pY)
-        {
-            y2= pY;
-        }
-        public float getX2()
-        {
-            return x2;
-        }
-        public float getY2()
-        {
-            return y2;
-        }
+        public float getX(){ return x; }
+        public float getX1() { return x1; }
+        public float getX2() { return x2; }
+        public float getX3() { return x3; }
+        public float getX4() { return x4; }
 
+        public float getY() { return y; }
+        public float getY1() { return y1; }
+        public float getY2() { return y2; }
+        public float getY3() { return y3; }
+        public float getY4() { return y4; }
+ 
+        public void setId(int pid) {id = pid;  }
+        public void setId1(int pid) { id1 = pid; }
+        public void setId2(int pid) { id2 = pid; }
+        public void setId3(int pid) { id3 = pid; }
+        public void setId4(int pid) { id4 = pid; }
 
+        public int getId() { return id; }
+        public int getId1() { return id1; }
+        public int getId2() { return id2; }
+        public int getId3() { return id3; }
+        public int getId4() { return id4; }
     }
 }
